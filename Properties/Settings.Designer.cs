41398e584dfe86f6ee6ba3c82e1521f1c11b11e0
Emanuele Disco <emawind84@gmail.com>
2026-03-24 16:17:47 +0900

    update

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace pmis.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("https://nppia.kspmis.com")]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://coupang.kspmis.com")]
         public string pmis_api_url {
             get {
                 return ((string)(this["pmis_api_url"]));
@@ -37,7 +37,7 @@ namespace pmis.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("CAICAIR")]
+        [global::System.Configuration.DefaultSettingValueAttribute("ULS1")]
         public string pmis_project_code {
             get {
                 return ((string)(this["pmis_project_code"]));
@@ -309,7 +309,7 @@ namespace pmis.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("data/caicair.db")]
+        [global::System.Configuration.DefaultSettingValueAttribute("data/uls1.db")]
         public string sqlite_db_location {
             get {
                 return ((string)(this["sqlite_db_location"]));
@@ -377,7 +377,7 @@ namespace pmis.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Techo International Airport Project")]
+        [global::System.Configuration.DefaultSettingValueAttribute("울산 Sub-HUB센터 신축공사")]
         public string current_profile {
             get {
                 return ((string)(this["current_profile"]));
